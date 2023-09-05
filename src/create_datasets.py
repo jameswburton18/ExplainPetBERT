0380db030937908bb595f17ff0f3acf32e3c77db
James Burton <james.burton@durham.ac.uk>
2023-09-05 14:48:04 +0100

    new

---
+++ src/create_datasets.py
@@ -0,0 +1,122 @@
+from sklearn.preprocessing import OrdinalEncoder
+import yaml
+import pandas as pd
+from src.helper_functions import Config
+from datasets import Dataset, DatasetDict
+
+# ## Dataset creation here
+for month in [
+    1,
+]:
+    i = 28 * month
+    di = Config("configs/dataset_info.yaml")
+    df_train = pd.read_csv("data/raw/train.csv")
+    df_test = pd.read_csv("data/raw/test.csv")
+
+    # rename each of di.numerical_cols_long to di.numerical_cols
+    for long_col, col in zip(di.numerical_cols_long, di.numerical_cols):
+        df_train = df_train.rename(columns={long_col: col})
+        df_test = df_test.rename(columns={long_col: col})
+    # Filter on age (young: 0-2, middle: 2-10, senior: 10+)
+    df_train = df_train[(df_train["age_at_consult"] > 10)]
+    df_test = df_test[(df_test["age_at_consult"] > 10)]
+
+    df_train["Difference"] = df_train["Difference"].astype(str)
+    df_train["Difference"] = df_train["Difference"].str.split(" ").str[0]
+    df_train["Difference"] = df_train["Difference"].str.replace("nan", "999")
+    df_train["Difference"] = df_train["Difference"].astype(int)
+
+    df_test["Difference"] = df_test["Difference"].astype(str)
+    df_test["Difference"] = df_test["Difference"].str.split(" ").str[0]
+    df_test["Difference"] = df_test["Difference"].str.replace("nan", "999")
+    df_test["Difference"] = df_test["Difference"].astype(int)
+
+    df_train_cases = df_train[
+        (df_train["Difference"] >= i) & (df_train["Difference"] < i + 28)
+    ]
+    df_test_cases = df_test[
+        (df_test["Difference"] >= i) & (df_test["Difference"] < i + 28)
+    ]
+
+    df_train_controls = df_train[df_train["Difference"] == 999].sample(
+        len(df_train_cases)
+    )
+    df_test_controls = df_test[df_test["Difference"] == 999].sample(len(df_test_cases))
+
+    df_train_sample = pd.concat([df_train_cases, df_train_controls]).dropna()
+    df_test_sample = pd.concat([df_test_cases, df_test_controls]).dropna()
+
+    # drop columns that are not in di.text_cols, di.numerical_cols, di.categorical_cols
+    all_cols = di.numerical_cols + di.categorical_cols + di.text_cols + [di.label_col]
+    df_train_sample = df_train_sample[all_cols]
+    df_test_sample = df_test_sample[all_cols]
+
+    train_ds = Dataset.from_pandas(df_train_sample, preserve_index=False)
+    train_ds = train_ds.class_encode_column(di.label_col)
+    test_ds = Dataset.from_pandas(df_test_sample, preserve_index=False)
+    test_ds = test_ds.class_encode_column(di.label_col)
+    train_ds = train_ds.train_test_split(
+        test_size=0.15, seed=42, stratify_by_column=di.label_col
+    )
+
+    ds = DatasetDict(
+        {"train": train_ds["train"], "validation": train_ds["test"], "test": test_ds}
+    )
+
+    # Now we have made the split but still need to deal with missing values, and that depends on the column type
+
+    # All as text
+    train_all_text = ds["train"].to_pandas()
+    val_all_text = ds["validation"].to_pandas()
+    test_all_text = ds["test"].to_pandas()
+
+    ds_all_text = DatasetDict(
+        {
+            "train": Dataset.from_pandas(
+                train_all_text.astype("str"), preserve_index=False
+            ),
+            "validation": Dataset.from_pandas(
+                val_all_text.astype("str"), preserve_index=False
+            ),
+            "test": Dataset.from_pandas(
+                test_all_text.astype("str"), preserve_index=False
+            ),
+        }
+    )
+
+    ds_all_text.push_to_hub(f"vet_month_{month}_all_text")
+
+    train = ds["train"].to_pandas()
+    val = ds["validation"].to_pandas()
+    test = ds["test"].to_pandas()
+
+    train[di.text_cols] = train[di.text_cols].astype("str")
+    val[di.text_cols] = val[di.text_cols].astype("str")
+    test[di.text_cols] = test[di.text_cols].astype("str")
+
+    # ds.push_to_hub(dataset_name)
+    if len(di.categorical_cols) > 0:
+        train[di.categorical_cols] = train[di.categorical_cols].astype("category")
+
+        enc = OrdinalEncoder(
+            encoded_missing_value=-1,
+            handle_unknown="use_encoded_value",
+            unknown_value=-1,
+        )
+        train[di.categorical_cols] = enc.fit_transform(train[di.categorical_cols])
+
+        val[di.categorical_cols] = val[di.categorical_cols].astype("category")
+        val[di.categorical_cols] = enc.transform(val[di.categorical_cols])
+
+        test[di.categorical_cols] = test[di.categorical_cols].astype("category")
+        test[di.categorical_cols] = enc.transform(test[di.categorical_cols])
+
+    ds2 = DatasetDict(
+        {
+            "train": Dataset.from_pandas(train),
+            "validation": Dataset.from_pandas(val),
+            "test": Dataset.from_pandas(test),
+        }
+    )
+
+    ds2.push_to_hub(f"vet_month_{month}_ordinal")
---
+++ src/helper_functions.py
@@ -0,0 +1,41 @@
+import yaml
+
+
+class Config:
+    def __init__(self, yaml_file_path):
+        with open(yaml_file_path, "r") as yaml_file:
+            yaml_data = yaml.safe_load(yaml_file)
+            for key, value in yaml_data.items():
+                setattr(self, key, value)
+
+
+def row_to_string(row, cols):
+    row["text"] = " | ".join(f"{col}: {row[col]}" for col in cols)
+    return row
+
+
+def prepare_text(dataset, di, version):
+    """This is all for preparing the text part of the dataset
+    Could be made more robust by referring to dataset_info.py instead"""
+
+    if version == "all_as_text":
+        cols = di.categorical_cols + di.numerical_cols + di.text_cols
+        dataset = dataset.map(row_to_string, fn_kwargs={"cols": cols})
+        return dataset
+    elif version == "text_col_only":
+        cols = di.text_cols
+        dataset = dataset.map(row_to_string, fn_kwargs={"cols": cols})
+        return dataset
+    # elif version == "all_as_text_base_reorder":
+    #     cols = di.base_reorder_cols[model_code]
+    #     cols = cols[::-1] if reverse else cols
+    #     dataset = dataset.map(row_to_string, fn_kwargs={"cols": cols})
+    #     return dataset
+    # elif version == "all_as_text_tnt_reorder":
+    #     cols = di.tnt_reorder_cols[model_code]
+    #     cols = cols[::-1] if reverse else cols
+    #     dataset = dataset.map(row_to_string, fn_kwargs={"cols": cols})
+    #     return dataset
+
+    else:
+        raise ValueError(f"Unknown dataset type version ({version}) combination")
